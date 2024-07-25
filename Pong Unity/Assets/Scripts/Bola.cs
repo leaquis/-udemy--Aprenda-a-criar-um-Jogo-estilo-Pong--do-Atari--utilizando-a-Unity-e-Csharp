a3bfa5e65100bc8da4025fe55284db0cfeb1090b
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-07-25 02:27:27 -0300

    Todos arquivos do jogo

---
+++ Pong Unity/Assets/Scripts/Bola.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+
+    public float velocidadeDaBola;
+    public Rigidbody2D oRigidBody2D;
+    public AudioSource somDaBola;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oRigidBody2D.velocity = new Vector2(velocidadeDaBola, velocidadeDaBola) * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collisionInfo) {
+        somDaBola.Play();
+    }
+}
---
+++ Pong Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public int pontuacaoDoJogador1;
+    public int pontuacaoDoJogador2;
+
+    public Text TextoDaPontuacao;
+
+    public AudioSource somDoGol;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reiniciar();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Fechar();
+        }
+    }
+
+    public void AumentarPontuacaoDoJogador1() {
+        pontuacaoDoJogador1 += 1;
+        AtualizarTextoDePontuacao();
+    }
+
+    public void AumentarPontuacaoDoJogador2() {
+        pontuacaoDoJogador2 += 1;
+        AtualizarTextoDePontuacao();
+    }
+
+    public void AtualizarTextoDePontuacao() {
+        TextoDaPontuacao.text = pontuacaoDoJogador1 + " X " + pontuacaoDoJogador2;
+        somDoGol.Play();
+    }
+
+    private void Reiniciar() {     
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Fechar() {
+       Application.Quit();
+    }
+}
